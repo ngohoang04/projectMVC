a23383b1293e535194ce8c8c12ae833e88ab21af
ngohoang04 <hoangngotrong2004@gmail.com>
2024-12-12 01:19:57 +0700

    Initial commit

---
+++ TequliqsRestaurant/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TequliasRestaurant.Models;
+
+namespace TequliqsRestaurant.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
---
+++ TequliqsRestaurant/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using TequliqsRestaurant.Models;
+
+namespace TequliasRestaurant.Models
+{
+    public class Order //Domain Model
+    {
+        public Order()
+        {
+            OrderItems = new List<OrderItem>();
+        }
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string? UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public decimal TotalAmount { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
